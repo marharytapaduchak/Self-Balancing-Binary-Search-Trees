8d56218be479cd159b668f035112fad40311f971
Marharyta <paduchak.pn@ucu.edu.ua>
2025-04-29 17:06:32 +0300

    b-tree inorder, preorder, postorder

--- b_tree.py
+++ b_tree.py
@@ -29,9 +29,8 @@ class BTree(AbstractTree):
     def __init__(self, key_col: int, t: int):
         super().__init__(key_col)
         # Minimum degree (defines max and min keys per node)
-        self.t: int = t
-        # Initialize tree with an empty leaf root
-        self._root: BTreeNode = BTreeNode(leaf=True)
+        self.t = t
+        self._root = BTreeNode(leaf=True)
 
     @property
     def root(self) -> BTreeNode | None:
@@ -44,18 +43,18 @@ class BTree(AbstractTree):
         """
         t = self.t
         y = x.child[i]
-        # Create new node z to hold y's right half
+        # Create new node to hold y's right half
         z = BTreeNode(leaf=y.leaf)
         # Median entry to move up
         median = y.keys[t - 1]
-        # Split y's keys
+
         z.keys = y.keys[t:]
         y.keys = y.keys[:t - 1]
-        # If not leaf, split children too
+
         if not y.leaf:
             z.child = y.child[t:]
             y.child = y.child[:t]
-        # Insert z as new child of x, and median into x.keys
+
         x.child.insert(i + 1, z)
         x.keys.insert(i, median)
 
@@ -77,19 +76,16 @@ class BTree(AbstractTree):
         k = data_entry.columns[self._key_col]
         i = len(x.keys) - 1
 
-        # find the correct spot in the leaf to insert the key
         if x.leaf:
             x.keys.append(data_entry)
             while i >= 0 and k < x.keys[i].columns[self._key_col]:
                 x.keys[i + 1] = x.keys[i]
                 i -= 1
             x.keys[i + 1] = data_entry
-        # if not a leaf, find the correct subtree to insert the key
         else:
             while i >= 0 and k < x.keys[i].columns[self._key_col]:
                 i -= 1
             i += 1
-            # if child node is full, split it
             if len(x.child[i].keys) == (2 * self.t) - 1:
                 self.split_child(x, i)
                 if k > x.keys[i].columns[self._key_col]:
@@ -109,13 +105,51 @@ class BTree(AbstractTree):
 
         if i < len(node.keys) and key == node.keys[i].columns[self._key_col]:
             return (node, i)
-        elif node.leaf:
+        if node.leaf:
             return None
-        else:
-            return self.find(key, node.child[i])
+        return self.find(key, node.child[i])
 
 
     def erase(self, key) -> None:
         """
         Erases data entries from tree by key
         """
+
+    def inorder(self):
+        return self._inorder(self._root)
+
+    def _inorder(self, node):
+        if node is None:
+            return []
+        res = []
+        for i, key in enumerate(node.keys):
+            if i < len(node.child):
+                res.extend(self._inorder(node.child[i]))
+            res.append(key)
+        if len(node.child) > len(node.keys):
+            res.extend(self._inorder(node.child[-1]))
+        return res
+
+    def preorder(self):
+        return self._preorder(self._root)
+
+    def _preorder(self, node):
+        if node is None:
+            return []
+        res = []
+        res.extend(node.keys)
+        for c in node.child:
+            res.extend(self._preorder(c))
+        return res
+
+    def postorder(self):
+        return self._postorder(self._root)
+
+    def _postorder(self, node):
+        if node is None:
+            return []
+        res = []
+        for c in node.child:
+            res.extend(self._postorder(c))
+        res.extend(node.keys)
+        return res
